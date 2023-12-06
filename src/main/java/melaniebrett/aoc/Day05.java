b93424a78f0c560e71361cb10ec93f9d050ccefa
Brooke Lear <brookel@spotify.com>
2023-12-06 09:49:45 +0000

    :white_check_mark: Day 05 part 1

--- src/main/java/melaniebrett/aoc/Day05.java
+++ src/main/java/melaniebrett/aoc/Day05.java
@@ -2,15 +2,15 @@ package melaniebrett.aoc;
 
 import static java.util.stream.Collectors.toMap;
 
+import com.google.common.collect.Range;
 import java.nio.file.Path;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
 import java.util.regex.MatchResult;
 import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
-import melaniebrett.aoc.models.DoublePair;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -23,8 +23,8 @@ public class Day05 {
     List<String> inputs = Utils.getInputLines(inputPath);
 
     logger.info("Day " + DAY);
-    logger.info("Part 1: {}", this.part1(inputs));
-    //    Part 1:
+    logger.info("Part 1: {}", String.format("%f", this.part1(inputs)));
+    //    Part 1: 173706076
     logger.info("Part 2: {}", this.part2(inputs));
     //    Part 2:
   }
@@ -37,24 +37,24 @@ public class Day05 {
             .map(Double::parseDouble)
             .toList();
 
-    Map<Double, Double> seedToSoil = getMapInput("seed-to-soil", input);
-    Map<Double, Double> soilToFertilizer = getMapInput("soil-to-fertilizer", input);
-    Map<Double, Double> fertilizerToWater = getMapInput("fertilizer-to-water", input);
-    Map<Double, Double> waterToLight = getMapInput("water-to-light", input);
-    Map<Double, Double> lightToTemperature = getMapInput("light-to-temperature", input);
-    Map<Double, Double> temperatureToHumidity = getMapInput("temperature-to-humidity", input);
-    Map<Double, Double> humidityToLocation = getMapInput("humidity-to-location", input);
+    Map<Range<Double>, Double> seedToSoil = getMapInput("seed-to-soil", input);
+    Map<Range<Double>, Double> soilToFertilizer = getMapInput("soil-to-fertilizer", input);
+    Map<Range<Double>, Double> fertilizerToWater = getMapInput("fertilizer-to-water", input);
+    Map<Range<Double>, Double> waterToLight = getMapInput("water-to-light", input);
+    Map<Range<Double>, Double> lightToTemperature = getMapInput("light-to-temperature", input);
+    Map<Range<Double>, Double> temperatureToHumidity =
+        getMapInput("temperature-to-humidity", input);
+    Map<Range<Double>, Double> humidityToLocation = getMapInput("humidity-to-location", input);
 
     return seeds.stream()
-        .map(seed -> seedToSoil.getOrDefault(seed, seed))
-        .map(soil -> soilToFertilizer.getOrDefault(soil, soil))
-        .map(fertilizer -> fertilizerToWater.getOrDefault(fertilizer, fertilizer))
-        .map(water -> waterToLight.getOrDefault(water, water))
-        .map(light -> lightToTemperature.getOrDefault(light, light))
-        .map(temperature -> temperatureToHumidity.getOrDefault(temperature, temperature))
-        .map(humidity -> humidityToLocation.getOrDefault(humidity, humidity))
+        .map(seed -> getDestinationFromSource(seed, seedToSoil))
+        .map(soil -> getDestinationFromSource(soil, soilToFertilizer))
+        .map(fertilizer -> getDestinationFromSource(fertilizer, fertilizerToWater))
+        .map(water -> getDestinationFromSource(water, waterToLight))
+        .map(light -> getDestinationFromSource(light, lightToTemperature))
+        .map(temperature -> getDestinationFromSource(temperature, temperatureToHumidity))
+        .map(humidity -> getDestinationFromSource(humidity, humidityToLocation))
         .mapToDouble(l -> l)
-        //        .mapToInt(l -> l)
         .min()
         .orElse(0);
   }
@@ -63,35 +63,46 @@ public class Day05 {
     return 0;
   }
 
-  private static Map<Double, Double> getMapInput(String mapType, String input) {
+  private static Map<Range<Double>, Double> getMapInput(String mapType, String input) {
     Pattern mapPattern =
-        Pattern.compile(String.format("(?s)(?<=%s map:\\n).*?(?=\\n\\n)", mapType), Pattern.DOTALL);
+        Pattern.compile(
+            String.format("(?s)(?<=%s map:\\n).*?(?=\\n\\n|\\Z)", mapType), Pattern.DOTALL);
 
     return mapPattern
         .matcher(input)
         .results()
         .map(MatchResult::group)
-        .flatMap(x -> getSourceToDestinationMap(List.of(x.split("\n"))).entrySet().stream())
+        .flatMap(x -> getSourceRangeToDestination(List.of(x.split("\n"))).entrySet().stream())
         .collect(toMap(Entry::getKey, Entry::getValue));
   }
 
-  private static Map<Double, Double> getSourceToDestinationMap(List<String> inputs) {
+  private static Map<Range<Double>, Double> getSourceRangeToDestination(List<String> inputs) {
     return inputs.stream()
-        .flatMap(
-            entry -> {
-              String[] entries = entry.split(" ");
-              double destination = Double.parseDouble(entries[0]);
-              double source = Double.parseDouble(entries[1]);
-              double range = Double.parseDouble(entries[2]);
-              List<DoublePair> pairs = new ArrayList<>();
-              for (int i = 0; i < range; i++) {
-                pairs.add(new DoublePair(source + i, destination + i));
-              }
-              return pairs.stream();
-            })
-        .collect(toMap(DoublePair::first, DoublePair::second));
+        .collect(
+            Collectors.toMap(
+                entry -> {
+                  String[] entries = entry.split(" ");
+                  double source = Double.parseDouble(entries[1]);
+                  double range = Double.parseDouble(entries[2]);
+                  return Range.closed(source, source + range);
+                },
+                entry -> {
+                  String[] entries = entry.split(" ");
+                  return Double.parseDouble(entries[0]);
+                }));
   }
-  //  private static Integer getDestination(Integer source, Map<Integer, Integer> map) {
-  //    return map.getOrDefault(source, source);
-  //  }
+
+  private static Double getDestinationFromSource(
+      Double source, Map<Range<Double>, Double> RangeMap) {
+    return RangeMap.entrySet().stream()
+        .filter(entry -> entry.getKey().contains(source))
+        .map(
+            entry -> {
+              Double start = entry.getKey().lowerEndpoint();
+              Double placeInRange = source - start;
+              return entry.getValue() + placeInRange;
+            })
+        .findFirst()
+        .orElse(source);
+  }
 }
